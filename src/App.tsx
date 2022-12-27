fe4f870628181d308ce3a191419a98cb46a575ad
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-12-26 20:34:31 -0600

    website started basic layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const projects = [
+  {
+    name: "taylormadetech.net",
+    description: "company website",
+    techstack: "javascript (react), hosted on AWS with Amplify",
+  },
+  {
+    name: "secrets.taylormadetech.net",
+    description: "secret sharing website",
+    techstack: "javascript (react), hosted on AWS with Amplify",
+  },
+  {
+    name: "mgdrywall-usa.com",
+    description: "MG Drywall U.S.A company website",
+    techstack: "javascript (next.js), hosted on AWS with Amplify",
+  },
+  {
+    name: "discord-bot",
+    description: "discord bot project",
+    techstack: "node.js",
+  },
+  {
+    name: "myRides",
+    description: "an app to book your favorite Uber or Lyft driver",
+    techstack: "javascript (react) front end, python (django) backend",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="font-nunito max-w-6xl mx-auto">
+      <header className="text-gray-900 pt-8 md:pt-16 capitalize text-center lg:flex lg:align-middle justify-center mx-4">
+        <div className="my-auto lg:grow">
+          <p className="text-4xl md:text-5xl">hector alvarez</p>
+          <p className="text-lg">full stack development & UX \ UI</p>
+        </div>
+        <img
+          src="/tmtn.png"
+          alt="TMTN logo"
+          className="max-w-md m-auto w-full"
+        />
       </header>
-    </div>
+      <h2 className="mx-4 text-2xl font-mplus font-bold">Project Catalog</h2>
+      {projects.map((project) => {
+        return (
+          <section
+            key={project.name}
+            className="p-2 bg-gray-800 m-4 text-gray-100 md:flex md:align-middle md:justify-center"
+          >
+            <div className="p-4 flex-grow">
+              <h3 className="text-xl md:text-3xl">{project.name}</h3>
+              <p className="text-sm md:text-lg">{project.description}</p>
+              <p className="pb-2 text-xs text-white">
+                tech stack: {project.techstack}
+              </p>
+            </div>
+            <div className="h-16 bg-gray-200 flex justify-around md:h-20 m-4">
+              <img
+                src="github.svg"
+                alt="github svg icon"
+                className="py-2 px-6 lg:hover:bg-white lg:hover:cursor-pointer"
+              />
+              <img
+                src="figma.svg"
+                alt="figma svg icon"
+                className="py-2 px-6 lg:hover:bg-white lg:hover:cursor-pointer"
+              />
+              <img
+                src="website.svg"
+                alt="website svg icon"
+                className="py-2 px-6 lg:hover:bg-white lg:hover:cursor-pointer"
+              />
+            </div>
+          </section>
+        );
+      })}
+      <footer className="text-lg text-center p-4">copyright 2022</footer>
+    </main>
   );
 }
 
