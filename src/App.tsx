79bb75a2488a41964c1e037e954bc43f2b80be15
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-01-03 16:18:28 +0000

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
 import React from "react";
-
-import Header from "./components/Header";
-import ProjectList from "./components/ProjectList";
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  return (
-    <main className="font-nunito max-w-6xl mx-auto">
-      <Header />
-      <ProjectList />
-      <footer className="text-lg text-center p-4">
-        <p>
-          check out my{" "}
-          <a
-            href="https://github.com/Hectormalvarez"
-            target="_blank"
-            rel="noreferrer"
-            className="text-blue-600 underline"
-          >
-            github
-          </a>
-        </p>
-        <p>copyright 2022</p>
-      </footer>
-    </main>
-  );
+  return <Outlet />;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route index element={<Home />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
