e47efc875d3abaab4009801fc408e210fb6e2e6a
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-01-07 11:07:28 -0600

    updated to match name updates

--- src/components/AddProject.tsx
+++ src/components/AddProject.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+
 import { useAdmin } from '../context/AdminContext'
 import { ProjectInt } from '../context/AdminContext'
 
 const AddProject = () => {
-  const { handleProjectData, addingProjectHandler } = useAdmin()
+  const { projectDataHandler, addingProjectHandler } = useAdmin()
+
   const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm<ProjectInt>()
+
   const onSubmit = handleSubmit((data) => {
     console.log(data)
-    handleProjectData(data)
+    projectDataHandler(data)
     addingProjectHandler()
     reset()
   })
@@ -21,6 +24,7 @@ const AddProject = () => {
   return (
     <form onSubmit={onSubmit} className='m-4 flex flex-col border-2 border-gray-800 p-2'>
       <h3 className='text-lg font-bold capitalize'>add a new project</h3>
+
       <label>Name</label>
       <input className='border-2 border-gray-800 p-2' {...register('name', { required: true })} />
       {errors.name && 'Name Required!'}
@@ -38,7 +42,7 @@ const AddProject = () => {
         {...register('techstack', { required: true })}
       />
       {errors.name && 'TechStack Required!'}
-
+      
       <button className='my-2 border-2 border-gray-900 bg-gray-800 p-2 text-gray-50 hover:bg-gray-200 hover:fill-white hover:text-white'>
         submit
       </button>
