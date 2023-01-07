b1a507402d5ce352a53bd3b696d2661af0ce420f
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-01-07 11:51:47 -0600

    added amplify to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Amplify } from 'aws-amplify'
 
 import { AdminProvider } from './context/AdminContext'
 import Home from './components/Home'
+
+import awsExports from './aws-exports'
+Amplify.configure(awsExports)
 
 function App() {
   return (
