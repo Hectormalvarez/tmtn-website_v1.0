025af9501552424d8f46362e5fd64fe1b17559d0
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-01-20 13:20:06 -0600

    updated add svg's

--- src/components/AddProjectButton.tsx
+++ src/components/AddProjectButton.tsx
@@ -20,8 +20,18 @@ const AddProjectButton = () => {
       className='flex border-2 border-gray-900 p-2 hover:bg-gray-900 hover:fill-white hover:text-white'
       onClick={() => dispatch({ type: EAdminAction.ADDING_PROJECT, payload: true })}
     >
-      <svg className='w-6 p-1 lg:w-8' viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg'>
-        <path d='M576.477 448.471l349.649.621c35.346.041 63.967 28.728 63.926 64.074s-28.728 63.967-64.074 63.926l-349.429-.621.194 349.647c.013 35.346-28.63 64.01-63.977 64.023s-64.01-28.63-64.023-63.977l-.195-349.921-349.622-.621C63.58 575.581 34.959 546.894 35 511.548s28.728-63.967 64.074-63.926l349.402.621-.194-349.361c-.013-35.346 28.63-64.01 63.977-64.023s64.01 28.63 64.023 63.977l.194 349.635z' />
+      <svg
+        className='w-6 p-1 lg:w-8 fill-white'
+        viewBox='0 0 24 24'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M14 7V13M11 10H17M14 21C11 21 8 21 5 21C3.89543 21 3.00001 20.1069 3.00001 19.0023C3 16.2888 3 11.5934 3 10M9 17H19C20.1046 17 21 16.1046 21 15V5C21 3.89543 20.1046 3 19 3H9C7.89543 3 7 3.89543 7 5V15C7 16.1046 7.89543 17 9 17Z'
+          stroke='#000000'
+          strokeWidth={2}
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
       </svg>
       Add Project
     </button>
--- src/components/ProjectLinks.tsx
+++ src/components/ProjectLinks.tsx
@@ -20,21 +20,27 @@ const ProjectLinks: React.FC<{ project: IProject }> = ({ project }) => {
     return (
       <div className='m-2 flex flex-col bg-gray-200 p-2'>
         <div className='flex flex-row-reverse justify-around'>
-
           {/* do not show add link if form is showing */}
           {!showLinkForm && (
             <div
-              className='text-center text-black lg:hover:cursor-pointer lg:hover:bg-white my-auto w-28 py-2'
+              className='my-auto w-28 py-2 text-center capitalize font-bold text-black lg:hover:cursor-pointer lg:hover:bg-white'
               onClick={() => {
                 dispatch({ type: EAdminAction.ADDING_LINK, projectID: project.id })
               }}
             >
               <svg
                 className='mx-auto w-8 lg:w-16'
-                viewBox='0 0 1024 1024'
+                viewBox='0 0 24 24'
+                fill='none'
                 xmlns='http://www.w3.org/2000/svg'
               >
-                <path d='M576.477 448.471l349.649.621c35.346.041 63.967 28.728 63.926 64.074s-28.728 63.967-64.074 63.926l-349.429-.621.194 349.647c.013 35.346-28.63 64.01-63.977 64.023s-64.01-28.63-64.023-63.977l-.195-349.921-349.622-.621C63.58 575.581 34.959 546.894 35 511.548s28.728-63.967 64.074-63.926l349.402.621-.194-349.361c-.013-35.346 28.63-64.01 63.977-64.023s64.01 28.63 64.023 63.977l.194 349.635z' />
+                <path
+                  d='M12 8V16M16 12H8M6 20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20Z'
+                  stroke='#000000'
+                  strokeWidth={2}
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                />
               </svg>
               add link
             </div>
@@ -46,7 +52,6 @@ const ProjectLinks: React.FC<{ project: IProject }> = ({ project }) => {
         </div>
 
         {showLinkForm && <AddProjectLinkForm project={project} linkOptions={linkOptions} />}
-
       </div>
     )
   }
