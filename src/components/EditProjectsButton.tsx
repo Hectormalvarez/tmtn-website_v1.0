e2b8a76cf78ba8852ca7b6ff33afe8d428cb1f74
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-01-20 18:22:07 -0600

    added edit project button

---
+++ src/components/EditProjectsButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const EditProjectsButton = () => {
+  return (
+    <div
+      className='
+        flex cursor-pointer border-2 border-gray-900 p-2 duration-300 hover:shadow-lg hover:shadow-gray-600
+        '
+      onClick={() => console.log('EDIT PROJECTS')}
+    >
+      <svg
+        className='w-6 fill-white p-1 lg:w-8'
+        viewBox='0 0 24 24'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M12 4H6C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V12M9 15V12.5L17.75 3.75C18.4404 3.05964 19.5596 3.05964 20.25 3.75V3.75C20.9404 4.44036 20.9404 5.55964 20.25 6.25L15.5 11L11.5 15H9Z'
+          stroke='#000000'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </svg>
+      edit projects
+    </div>
+  )
+}
+
+export default EditProjectsButton
--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -3,6 +3,7 @@ import { useAdmin } from '../hooks/AdminContext'
 import AddProjectForm from './AddProjectForm'
 import AddProjectButton from './AddProjectButton'
 import Project from './Project'
+import EditProjectsButton from './EditProjectsButton'
 
 const ProjectList = () => {
   const { adminState, projectData } = useAdmin()
@@ -10,7 +11,10 @@ const ProjectList = () => {
     <section>
       <div className='font-mplus mx-4 flex justify-between font-bold lg:text-2xl'>
         <h2 className='p-2'>Project Catalog</h2>
-        <AddProjectButton />
+        <div className='flex gap-2'>
+          <EditProjectsButton />
+          <AddProjectButton />
+        </div>
       </div>
       {adminState.loggedIn && adminState.addingProject && <AddProjectForm />}
       {projectData.map((project) => {
