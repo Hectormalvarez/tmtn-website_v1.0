82de0e51067b6dd5dacbeadf69ddbadb9d602c04
Hector Alvarez <alvarez.hectorm@outlook.com>
2023-02-12 10:24:10 -0600

    added delete confirm

---
+++ src/components/DeleteProjectConfirm.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { IProject, useAdmin } from '../hooks/AdminContext'
+import { EAdminAction } from '../hooks/adminReducer'
+
+const DeleteProjectConfirm: React.FC<{ project: IProject }> = ({ project }) => {
+  const { dispatch } = useAdmin()
+
+  const handleDeleteOnClick = () => {
+    console.log(`deleting ${project.id}`)
+  }
+  const handleCancelOnClick = () => {
+    dispatch({ type: EAdminAction.SET_CURRENTLY_DELETING, setCurrentlyDeleting: null })
+  }
+
+  return (
+    <div>
+      <h3 className='tex p-2 text-2xl text-red-50 underline'>delete {project.name}?</h3>
+      <div className='flex justify-around'>
+        <div
+          onClick={handleDeleteOnClick}
+          className='flex-grow border-2 border-transparent bg-slate-500 py-2 text-center uppercase hover:cursor-pointer hover:border-red-800'
+        >
+          delete
+        </div>
+        <div
+          onClick={handleCancelOnClick}
+          className='flex-grow border-2 border-transparent bg-slate-500 py-2 text-center uppercase hover:cursor-pointer hover:border-white'
+        >
+          cancel
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DeleteProjectConfirm
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,6 +5,7 @@ import DeleteButton from './DeleteButton'
 import EditProjectForm from './EditProjectForm'
 import EditButton from './EditButton'
 import ProjectLinks from './ProjectLinks'
+import DeleteProjectConfirm from './DeleteProjectConfirm'
 
 interface ProjectProps {
   project: IProject
@@ -14,14 +15,19 @@ interface ProjectProps {
 const Project: React.FC<ProjectProps> = ({ project }) => {
   const { adminState } = useAdmin()
   const showEditForm = adminState.currentlyEditing.id == project.id
+  const showDeleteForm = adminState.currentlyDeleting.id == project.id
+  const showButtons =
+    adminState.editingProjects &&
+    adminState.currentlyEditing.id !== project.id &&
+    adminState.currentlyDeleting.id !== project.id
 
   return (
     <article key={project.name} className='m-4 bg-gray-800 p-2 text-white'>
-      <div className={`w-full ${adminState.currentlyEditing  ? "" : "lg:flex"}`}>
-        {adminState.editingProjects && adminState.currentlyEditing.id !== project.id && (
-          <div className='flex lg:flex-col gap-2 m-2'>
-              <EditButton type='project' id={project.id} />
-              <DeleteButton type='project' id={project.id} />
+      <div className={`w-full ${adminState.currentlyEditing ? '' : 'lg:flex'}`}>
+        {showButtons && (
+          <div className='m-2 flex gap-2 lg:flex-col'>
+            <EditButton type='project' id={project.id} />
+            <DeleteButton type='project' id={project.id} />
           </div>
         )}
         <div className='flex flex-col p-2 lg:flex-grow'>
@@ -29,7 +35,8 @@ const Project: React.FC<ProjectProps> = ({ project }) => {
           <p className='flex-grow text-sm lg:text-lg'>{project.description}</p>
           <p className='pb-2 text-xs text-white'>tech stack: {project.techstack}</p>
         </div>
-        <ProjectLinks project={project} />
+        {showDeleteForm && <DeleteProjectConfirm project={project} />}
+        {!showDeleteForm && <ProjectLinks project={project} />}
       </div>
       {showEditForm && <EditProjectForm project={project} />}
     </article>
@@ -37,3 +44,4 @@ const Project: React.FC<ProjectProps> = ({ project }) => {
 }
 
 export default Project
+
